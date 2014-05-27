74d8997950c143a18c9b6d6830d0ec76ace59498
Simon Wielens <simonwielens@gmail.com>
2014-05-28 00:27:56 +0100

    added 2 new events

--- ProductionProcessSpike/src/ie/simo/movies/generator/ProductionEventGenerator.java
+++ ProductionProcessSpike/src/ie/simo/movies/generator/ProductionEventGenerator.java
@@ -25,8 +25,8 @@ public class ProductionEventGenerator {
 				new ProductionEvent(1, 5, -10, "The star of your movie was pulled over for DUI and was jailed overnight. Production delayed while he arranges a lawyer.", false),
 				new ProductionEvent(5, 5, 0, "Your director got into a twitter argument with Kanye West. No one wins", false),
 				new ProductionEvent(0, 0, -15, "There was a fire at the special effects company's offices, and the work they did was destroyed and will have to be redone", false),
-				new ProductionEvent(1, 5, -2, "Your sound engineer has accidentally appeared on 'To catch a predator'. Production has been delayed while you get a replacement", false),
-				new ProductionEvent(1, 5, -2, "Your sound engineer has accidentally appeared on 'To catch a predator'. Production has been delayed while you get a replacement", false),
+				new ProductionEvent(1, 5, -2, "An early draft of your script was leaked on the internet, and was mercilessly mocked.", false),
+				new ProductionEvent(1, 5, -2, "An early cut of your movie was leaked, before special effects were added. It looks pretty ridiculous.", false),
 				new ProductionEvent(1, 5, -2, "Your sound engineer has accidentally appeared on 'To catch a predator'. Production has been delayed while you get a replacement", false),
 				new ProductionEvent(1, 5, -2, "Your sound engineer has accidentally appeared on 'To catch a predator'. Production has been delayed while you get a replacement", false)
 
